b124e6593eff0ec7a113a3b717822364044ef70e
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-04-14 09:25:09 -0400

    syntax

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ $on_mod(Loaded) {
 		std::ifstream file(pathRogers);
 		std::string technoblade;
 		while (std::getline(file, technoblade)) {
-			quotes.push_back(fmt.format("\"{}\"", technoblade));
+			quotes.push_back(fmt::format("\"{}\"", technoblade));
 		} // technically i can write two one-time use boolean variables to allow people to toggle these things on and off as they please without the quotes adding themselves multiple times into the vector, but i'd rather add the "restart required" barrier just to be extra safe
 	}
 
