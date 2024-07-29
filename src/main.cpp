1e61027073873782eaa25c089d24dacbd98c5ef5
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-07-28 20:23:40 -0400

    motorcycle engines are the worst invention ever

--- src/main.cpp
+++ src/main.cpp
@@ -182,7 +182,7 @@ class $modify(PlayLayer) {
 
 class $modify(MyCurrencyRewardLayer, CurrencyRewardLayer) {
 	bool init(int orbs, int stars, int moons, int diamonds, CurrencySpriteType demonKey, int keyCount, CurrencySpriteType shardType, int shardsCount, cocos2d::CCPoint position, CurrencyRewardType p9, float p10, float time) {
-		bool result = CurrencyRewardLayer::init(orbs, stars, moons, diamonds, demonkey, keyCount, shardType, shardsCount, position, p9, p10, time);
+		bool result = CurrencyRewardLayer::init(orbs, stars, moons, diamonds, demonKey, keyCount, shardType, shardsCount, position, p9, p10, time);
 		if (this->getParent()->getID() == "EndLevelLayer" && Mod::get()->getSettingValue<bool>("hideEndLevelLayer")) {
 			this->setVisible(false);
 		}
