6a67f65a3d7946442257e1bd1865040bad3d501b
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2026-07-10 23:06:56 -0400

    ugh fineeeee

--- src/MenuLayer.cpp
+++ src/MenuLayer.cpp
@@ -6,47 +6,41 @@
 
 using namespace geode::prelude;
 
-class $modify(MyMenuLayer, MenuLayer) {
-	bool init() {
-		if (!MenuLayer::init()) return false;
+$on_game(ModsLoaded) {
+	const auto mods = Loader::get()->getAllMods();
+	Manager* manager = managerMacro;
+	manager->totalMods = mods.size();
+	std::ranges::for_each(mods, [&](const Mod* mod) {
+		std::optional<LoadProblem> problem = mod->getLoadProblem();
+		if (!problem && mod->isLoaded()) manager->loadedMods += 1;
+		else if (!mod->isLoaded() && !problem) manager->disabledMods += 1;
 
-		const auto mods = Loader::get()->getAllMods();
-		Manager* manager = managerMacro;
-		manager->totalMods = mods.size();
-		std::ranges::for_each(mods, [&](const Mod* mod) {
-			std::optional<LoadProblem> problem = mod->getLoadProblem();
-			if (!problem && mod->isLoaded()) manager->loadedMods += 1;
-			else if (!mod->isLoaded() && !problem) manager->disabledMods += 1;
+		const std::string& modID = mod->getID();
 
-			const std::string& modID = mod->getID();
+		if (manager->modIDToModMenu.contains(modID)) manager->loadedModMenus.push_back(modID);
 
-			if (manager->modIDToModMenu.contains(modID)) manager->loadedModMenus.push_back(modID);
+		std::string formattedByline = "by ";
+		for (const std::string& developer : mod->getDevelopers()) {
+			formattedByline = formattedByline.append(fmt::format("{}, ", developer));
+		}
+		if (!formattedByline.empty() && utils::string::endsWith(formattedByline, ", ")) {
+			formattedByline.pop_back();
+			formattedByline.pop_back();
+		}
 
-			std::string formattedByline = "by ";
-			for (const std::string& developer : mod->getDevelopers()) {
-				formattedByline = formattedByline.append(fmt::format("{}, ", developer));
-			}
-			if (!formattedByline.empty() && utils::string::endsWith(formattedByline, ", ")) {
-				formattedByline.pop_back();
-				formattedByline.pop_back();
-			}
+		std::string formattedProblemsList;
+		if (problem && problem->isProblemTheUserShouldCareAbout()) {
+			manager->problemMods += 1;
+			formattedProblemsList = fmt::format(" {{<co>{}</c>}}", problem->message);
+		}
 
-			std::string formattedProblemsList;
-			if (problem && problem->isProblemTheUserShouldCareAbout()) {
-				manager->problemMods += 1;
-				formattedProblemsList = fmt::format(" {{<co>{}</c>}}", problem->message);
-			}
+		const std::string& formattedModListItem = fmt::format(
+			"- {}{}</c> {} {} [{}]{}",
+			mod->isLoaded() ? "<c-FFFFFF>" : !problem ? "<c-AAAAAA>" : "<c-FF0000>",
+			mod->getName(), mod->getVersion().toVString(), formattedByline,
+			modID, formattedProblemsList
+		);
 
-			const std::string& formattedModListItem = fmt::format(
-				"- {}{}</c> {} {} [{}]{}",
-				mod->isLoaded() ? "<c-FFFFFF>" : !problem ? "<c-AAAAAA>" : "<c-FF0000>",
-				mod->getName(), mod->getVersion().toVString(), formattedByline,
-				modID, formattedProblemsList
-			);
-
-			manager->formattedModsListVector.push_back(formattedModListItem);
-		});
-
-		return true;
-	}
-};
+		manager->formattedModsListVector.push_back(formattedModListItem);
+	});
+}
